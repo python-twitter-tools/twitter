af37e278d5069aec046aaf9c533df583ccae1811
Mike Verdone <mike.verdone@gmail.com>
2010-05-08 20:20:24 +0200

    Dont require simplejson if python is 2.6 or better

--- setup.py
+++ setup.py
@@ -3,6 +3,18 @@ import sys, os
 
 version = '1.3b'
 
+install_requires=[
+    # -*- Extra requirements: -*-
+    "python-dateutil>=1.1",
+    ],
+
+def _py26OrGreater():
+    return sys.hexversion > 0x20600f0
+
+if not _py26OrGreater():
+    install_requires.append("simplejson>=1.7.1")
+
+
 setup(name='twitter',
       version=version,
       description="An API and command-line toolset for Twitter (twitter.com)",
@@ -28,11 +40,7 @@ setup(name='twitter',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=True,
-      install_requires=[
-          # -*- Extra requirements: -*-
-          "simplejson>=1.7.1",
-          "python-dateutil>=1.1",
-      ],
+      install_requires=install_requires,
       entry_points="""
       # -*- Entry points: -*-
       [console_scripts]
