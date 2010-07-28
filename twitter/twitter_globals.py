7ef05275d707e5a998f48ee2f54f325ab2f03d76
Mike Verdone <mike.verdone@gmail.com>
2010-07-28 14:30:23 +0200

    Version bump 1.4.2

--- twitter/twitter_globals.py
+++ twitter/twitter_globals.py
@@ -11,21 +11,21 @@ POST_ACTIONS = [
     'update', 'retweet',
 
     # Direct Message Methods
-    'new', 
+    'new',
 
     # Account Methods
-    'update_profile_image', 'update_delivery_device', 'update_profile', 
-    'update_profile_background_image', 'update_profile_colors', 
-    'update_location', 'end_session', 
+    'update_profile_image', 'update_delivery_device', 'update_profile',
+    'update_profile_background_image', 'update_profile_colors',
+    'update_location', 'end_session',
 
     # Notification Methods
-    'leave', 'follow', 
+    'leave', 'follow',
 
-    # Status Methods, Block Methods, Direct Message Methods, 
+    # Status Methods, Block Methods, Direct Message Methods,
     # Friendship Methods, Favorite Methods
-    'destroy', 
+    'destroy',
 
     # Block Methods, Friendship Methods, Favorite Methods
-    'create', 
+    'create',
 
 ]
