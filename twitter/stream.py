2300838f1d3eb7e6b5b339c19b2ec8447b178e3f
Mike Verdone <mike.verdone@gmail.com>
2011-12-03 19:16:49 +0100

    Implement true non-blocking TwitterStream.

--- twitter/stream.py
+++ twitter/stream.py
@@ -6,17 +6,22 @@ except ImportError:
     import urllib2 as urllib_request
     import urllib2 as urllib_error
 import json
+from ssl import SSLError
 
 from .api import TwitterCall, wrap_response
 
 class TwitterJSONIter(object):
 
-    def __init__(self, handle, uri, arg_data):
+    def __init__(self, handle, uri, arg_data, block=True):
         self.decoder = json.JSONDecoder()
         self.handle = handle
         self.buf = b""
+        self.block = block
 
     def __iter__(self):
+        sock = self.handle.fp._sock.fp._sock
+        if not self.block:
+            sock.setblocking(False)
         while True:
             try:
                 utf8_buf = self.buf.decode('utf8').lstrip()
@@ -25,16 +30,33 @@ class TwitterJSONIter(object):
                 yield wrap_response(res, self.handle.headers)
                 continue
             except ValueError as e:
-                pass
+                if self.block:
+                    pass
+                else:
+                    yield None
             except urllib_error.HTTPError as e:
                 raise TwitterHTTPError(e, uri, self.format, arg_data)
             # this is a non-blocking read (ie, it will return if any data is available)
-            self.buf += self.handle.fp._sock.fp._sock.recv(1024)
+            try:
+                self.buf += sock.recv(1024)
+            except SSLError as e:
+                if (not self.block) and (e.errno == 2):
+                    # Apparently this means there was nothing in the socket buf
+                    pass
+                else:
+                    raise
+
+def handle_stream_response(req, uri, arg_data, block):
+    handle = urllib_request.urlopen(req,)
+    return iter(TwitterJSONIter(handle, uri, arg_data, block))
 
 class TwitterStreamCall(TwitterCall):
     def _handle_response(self, req, uri, arg_data):
-        handle = urllib_request.urlopen(req,)
-        return iter(TwitterJSONIter(handle, uri, arg_data))
+        return handle_stream_response(req, uri, arg_data, block=True)
+
+class TwitterStreamCallNonBlocking(TwitterCall):
+    def _handle_response(self, req, uri, arg_data):
+        return handle_stream_response(req, uri, arg_data, block=False)
 
 class TwitterStream(TwitterStreamCall):
     """
@@ -51,14 +73,21 @@ class TwitterStream(TwitterStreamCall):
 
     The iterator will yield tweets forever and ever (until the stream
     breaks at which point it raises a TwitterHTTPError.)
+
+    The `bloc` paramater controls if the stream is blocking. 
     """
     def __init__(
         self, domain="stream.twitter.com", secure=True, auth=None,
-        api_version='1'):
+        api_version='1', block=True):
         uriparts = ()
         uriparts += (str(api_version),)
 
+        if block:
+            call_cls = TwitterStreamCall
+        else:
+            call_cls = TwitterStreamCallNonBlocking
+
         TwitterStreamCall.__init__(
             self, auth=auth, format="json", domain=domain,
-            callable_cls=TwitterStreamCall,
+            callable_cls=call_cls,
             secure=secure, uriparts=uriparts)
