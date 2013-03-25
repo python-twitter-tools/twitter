63d78cf0e8e75763127b99a6e06110c8ee193769
Mickael GOETZ <mickael.goetz@gmail.com>
2013-03-25 21:03:33 +0100

    fix __init_.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,4 @@
 # this file is here to make the importation of the project as a git-submodule possible
 # command example: git submodule add git://github.com/sixohsix/twitter.git twitter
+
+import twitterfrom .twitter import *
