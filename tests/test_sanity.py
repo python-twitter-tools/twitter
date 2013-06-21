4a803760de93e6bafbc2154e6d92606b03f9931d
Mike Verdone <mike.verdone@gmail.com>
2013-06-21 15:11:44 -0600

    Fix some tests.

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -10,13 +10,9 @@ noauth = NoAuth()
 oauth = OAuth(*read_token_file('tests/oauth_creds')
                + (CONSUMER_KEY, CONSUMER_SECRET))
 
-twitter = Twitter(domain='api.twitter.com',
-                  auth=oauth,
-                  api_version='1')
 twitter11 = Twitter(domain='api.twitter.com',
                     auth=oauth,
                     api_version='1.1')
-twitter_na = Twitter(domain='api.twitter.com', auth=noauth, api_version='1')
 twitter11_na = Twitter(domain='api.twitter.com', auth=noauth, api_version='1.1')
 
 
@@ -28,9 +24,9 @@ def get_random_str():
 
 def test_API_set_tweet():
     random_tweet = "A random tweet " + get_random_str()
-    twitter.statuses.update(status=random_tweet)
+    twitter11.statuses.update(status=random_tweet)
     time.sleep(2)
-    recent = twitter.statuses.user_timeline()
+    recent = twitter11.statuses.home_timeline()
     assert recent
     assert isinstance(recent.rate_limit_remaining, int)
     assert isinstance(recent.rate_limit_reset, int)
@@ -39,9 +35,9 @@ def test_API_set_tweet():
 
 def test_API_set_unicode_tweet():
     random_tweet = u"A random tweet with unicode üøπ" + get_random_str()
-    twitter.statuses.update(status=random_tweet)
+    twitter11.statuses.update(status=random_tweet)
 
-    recent = twitter.statuses.user_timeline()
+    recent = twitter11.statuses.home_timeline()
     assert recent
     assert random_tweet == recent[0]['text']
 
@@ -49,19 +45,6 @@ def test_API_set_unicode_tweet():
 def test_search():
     results = twitter11.search.tweets(q='foo')
     assert results
-
-
-def test_get_trends():
-    # This is one method of inserting parameters, using named
-    # underscore params.
-    world_trends = twitter.trends._woeid(_woeid=1)
-    assert world_trends
-
-
-def test_get_trends_2():
-    # This is a nicer variation of the same call as above.
-    world_trends = twitter.trends._(1)
-    assert world_trends
 
 
 def test_get_trends_3():
