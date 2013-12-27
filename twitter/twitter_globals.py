bd1346a318f8f8d553a3fbf0a353ef6d68102566
RouxRC <b.ooghe@gmail.com>
2013-12-27 10:38:15 +0100

    Use POST for all methods requiring it in specs
    Added all missing methods from https://dev.twitter.com/docs/api/1.1
    Also included some of the streaming methods which work with both GET
    and POST but accept arguments like "track" which can quickly require
    POST.
    
    (Closes #187 #145 #188)

--- twitter/twitter_globals.py
+++ twitter/twitter_globals.py
@@ -16,24 +16,29 @@ POST_ACTIONS = [
     # Account Methods
     'update_profile_image', 'update_delivery_device', 'update_profile',
     'update_profile_background_image', 'update_profile_colors',
-    'update_location', 'end_session',
+    'update_location', 'end_session', 'settings',
+    'update_profile_banner', 'remove_profile_banner',
 
     # Notification Methods
     'leave', 'follow',
 
     # Status Methods, Block Methods, Direct Message Methods,
     # Friendship Methods, Favorite Methods
-    'destroy',
+    'destroy', 'destroy_all',
 
     # Block Methods, Friendship Methods, Favorite Methods
     'create', 'create_all',
 
     # Users Methods
-    'lookup',
+    'lookup', 'report_spam',
 
-    # Semantic Methods
-    'filter',
+    # Geo Methods
+    'place',
+
+    # Streaming Methods
+    'filter', 'user', 'site',
 
     # OAuth Methods
-    'token',
+    'token', 'access_token',
+    'request_token', 'invalidate_token',
 ]
