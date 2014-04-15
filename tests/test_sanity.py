2cf5509c65d0c3657968a2698e1f4132ba5b6c43
hugovk <hugovk@users.noreply.github.com>
2014-04-15 12:36:11 +0300

    Add a test for API v1 unsupported. Other tests use v1.1 instead of v1.

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -8,28 +8,46 @@ from twitter.cmdline import CONSUMER_KEY, CONSUMER_SECRET
 
 noauth = NoAuth()
 oauth = OAuth(*read_token_file('tests/oauth_creds')
-               + (CONSUMER_KEY, CONSUMER_SECRET))
+              + (CONSUMER_KEY, CONSUMER_SECRET))
 
-twitter = Twitter(domain='api.twitter.com',
-                  auth=oauth,
-                  api_version='1')
+twitter1 = Twitter(domain='api.twitter.com',
+                   auth=oauth,
+                   api_version='1')
+
 twitter11 = Twitter(domain='api.twitter.com',
                     auth=oauth,
                     api_version='1.1')
-twitter_na = Twitter(domain='api.twitter.com', auth=noauth, api_version='1')
 
+twitter_na = Twitter(domain='api.twitter.com', auth=noauth, api_version='1.1')
 
 AZaz = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 
 def get_random_str():
     return ''.join(choice(AZaz) for _ in range(10))
 
 
+def test_v1_not_supported():
+    # Arrange
+    random_tweet = "Random tweet that shouldn't be posted " + get_random_str()
+
+    # Act
+    try:
+        twitter1.statuses.update(status=random_tweet)
+        time.sleep(2)
+        error = False
+    except:
+        error = True
+
+    # Assert
+    assert error
+
+
 def test_API_set_tweet():
     random_tweet = "A random tweet " + get_random_str()
-    twitter.statuses.update(status=random_tweet)
+    twitter11.statuses.update(status=random_tweet)
     time.sleep(2)
-    recent = twitter.statuses.user_timeline()
+    recent = twitter11.statuses.user_timeline()
     assert recent
     assert isinstance(recent.rate_limit_remaining, int)
     assert isinstance(recent.rate_limit_reset, int)
@@ -38,9 +56,9 @@ def test_API_set_tweet():
 
 def test_API_set_unicode_tweet():
     random_tweet = u"A random tweet with unicode üøπ" + get_random_str()
-    twitter.statuses.update(status=random_tweet)
+    twitter11.statuses.update(status=random_tweet)
 
-    recent = twitter.statuses.user_timeline()
+    recent = twitter11.statuses.user_timeline()
     assert recent
     assert random_tweet == recent[0]['text']
 
@@ -54,13 +72,13 @@ def test_search():
 def test_get_trends():
     # This is one method of inserting parameters, using named
     # underscore params.
-    world_trends = twitter.trends._woeid(_woeid=1)
+    world_trends = twitter11.trends._woeid(_woeid=1)
     assert world_trends
 
 
 def test_get_trends_2():
     # This is a nicer variation of the same call as above.
-    world_trends = twitter.trends._(1)
+    world_trends = twitter11.trends._(1)
     assert world_trends
 
 
@@ -68,3 +86,4 @@ def test_get_trends_3():
     # Of course they broke it all again in 1.1...
     assert twitter11.trends.place(_id=1)
 
+# End of file
