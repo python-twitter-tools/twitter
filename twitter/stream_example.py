d1942b9b8d92c82032c80e10d9d72efb8552aef7
Hugo <hugovk@users.noreply.github.com>
2016-11-05 21:50:57 +0200

    flake8

--- twitter/stream_example.py
+++ twitter/stream_example.py
@@ -9,30 +9,42 @@ import argparse
 
 from twitter.stream import TwitterStream, Timeout, HeartbeatTimeout, Hangup
 from twitter.oauth import OAuth
-from twitter.oauth2 import OAuth2, read_bearer_token_file
 from twitter.util import printNicely
+
 
 def parse_arguments():
 
     parser = argparse.ArgumentParser(description=__doc__ or "")
 
-    parser.add_argument('-t',  '--token', required=True, help='The Twitter Access Token.')
-    parser.add_argument('-ts', '--token-secret', required=True, help='The Twitter Access Token Secret.')
-    parser.add_argument('-ck', '--consumer-key', required=True, help='The Twitter Consumer Key.')
-    parser.add_argument('-cs', '--consumer-secret', required=True, help='The Twitter Consumer Secret.')
-    parser.add_argument('-us', '--user-stream', action='store_true', help='Connect to the user stream endpoint.')
-    parser.add_argument('-ss', '--site-stream', action='store_true', help='Connect to the site stream endpoint.')
-    parser.add_argument('-to', '--timeout', help='Timeout for the stream (seconds).')
-    parser.add_argument('-ht', '--heartbeat-timeout', help='Set heartbeat timeout.', default=90)
-    parser.add_argument('-nb', '--no-block', action='store_true', help='Set stream to non-blocking.')
-    parser.add_argument('-tt', '--track-keywords', help='Search the stream for specific text.')
+    parser.add_argument('-t',  '--token', required=True,
+                        help='The Twitter Access Token.')
+    parser.add_argument('-ts', '--token-secret', required=True,
+                        help='The Twitter Access Token Secret.')
+    parser.add_argument('-ck', '--consumer-key', required=True,
+                        help='The Twitter Consumer Key.')
+    parser.add_argument('-cs', '--consumer-secret', required=True,
+                        help='The Twitter Consumer Secret.')
+    parser.add_argument('-us', '--user-stream', action='store_true',
+                        help='Connect to the user stream endpoint.')
+    parser.add_argument('-ss', '--site-stream', action='store_true',
+                        help='Connect to the site stream endpoint.')
+    parser.add_argument('-to', '--timeout',
+                        help='Timeout for the stream (seconds).')
+    parser.add_argument('-ht', '--heartbeat-timeout',
+                        help='Set heartbeat timeout.', default=90)
+    parser.add_argument('-nb', '--no-block', action='store_true',
+                        help='Set stream to non-blocking.')
+    parser.add_argument('-tt', '--track-keywords',
+                        help='Search the stream for specific text.')
     return parser.parse_args()
+
 
 def main():
     args = parse_arguments()
 
     # When using twitter stream you must authorize.
-    auth = OAuth(args.token, args.token_secret, args.consumer_key, args.consumer_secret)
+    auth = OAuth(args.token, args.token_secret,
+                 args.consumer_key, args.consumer_secret)
 
     # These arguments are optional:
     stream_args = dict(
@@ -45,10 +57,12 @@ def main():
         query_args['track'] = args.track_keywords
 
     if args.user_stream:
-        stream = TwitterStream(auth=auth, domain='userstream.twitter.com', **stream_args)
+        stream = TwitterStream(auth=auth, domain='userstream.twitter.com',
+                               **stream_args)
         tweet_iter = stream.user(**query_args)
     elif args.site_stream:
-        stream = TwitterStream(auth=auth, domain='sitestream.twitter.com', **stream_args)
+        stream = TwitterStream(auth=auth, domain='sitestream.twitter.com',
+                               **stream_args)
         tweet_iter = stream.site(**query_args)
     else:
         stream = TwitterStream(auth=auth, **stream_args)
