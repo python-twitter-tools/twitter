f3abd2488189d9762e718936c66c26a5dc4c39bc
Hugo <hugovk@users.noreply.github.com>
2019-10-12 18:09:31 +0300

    Drop support for EOL Python 2.6, 3.2 and 3.3

--- setup.py
+++ setup.py
@@ -9,12 +9,6 @@ except ImportError:
 
 version = '1.18.0'
 
-install_requires = [
-    # -*- Extra requirements: -*-
-    ]
-if sys.version_info < (2,7):
-    install_requires.append('argparse')
-
 tests_require = [
     'nose',
     ]
@@ -23,6 +17,7 @@ setup(name='twitter',
       version=version,
       description="An API and command-line toolset for Twitter (twitter.com)",
       long_description=long_description,
+      python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
       # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
           "Development Status :: 5 - Production/Stable",
@@ -31,14 +26,12 @@ setup(name='twitter',
           "Natural Language :: English",
           "Operating System :: OS Independent",
           "Programming Language :: Python :: 2",
-          "Programming Language :: Python :: 2.6",
           "Programming Language :: Python :: 2.7",
           "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.2",
-          "Programming Language :: Python :: 3.3",
           "Programming Language :: Python :: 3.4",
           "Programming Language :: Python :: 3.5",
           "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
           "Programming Language :: Python :: Implementation :: CPython",
           "Programming Language :: Python :: Implementation :: PyPy",
           "Topic :: Communications :: Chat :: Internet Relay Chat",
@@ -49,12 +42,11 @@ setup(name='twitter',
       keywords='twitter, IRC, command-line tools, web 2.0',
       author='Mike Verdone',
       author_email='mike.verdone+twitterapi@gmail.com',
-      url='http://mike.verdone.ca/twitter/',
+      url='https://mike.verdone.ca/twitter/',
       license='MIT License',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=True,
-      install_requires=install_requires,
       tests_require=tests_require,
       test_suite = 'nose.collector',
       entry_points="""
