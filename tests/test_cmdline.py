1968bb5cc97e7942e06e1685b2608b7463c98a2a
David Foucher <dev@tyjak.net>
2020-12-02 21:16:22 +0100

    Fix test

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -2,6 +2,8 @@
 
 import json
 import re
+import unittest
+from mock import patch
 
 try:
     import HTMLParser
@@ -16,40 +18,39 @@ from twitter.cmdline import (
 )
 
 
-status = {
-    "created_at": "sun dec 20 18:33:30 +0000 2020",
-    "user": {"screen_name": "myusername", "location": "Paris"},
-    "text": "test &amp; test",
-}
+class TestCmdLine(unittest.TestCase):
 
+    status = {
+        "created_at": "sun dec 20 18:33:30 +0000 2020",
+        "user": {"screen_name": "myusername", "location": "Paris, France"},
+        "text": "test &amp; test",
+    }
 
-def test_replaceInStatus():
-    status = "my&amp;status @twitter #tag"
-    assert replaceInStatus(status) == "my&status @twitter #tag"
+    def test_replaceInStatus(self):
+        status = "my&amp;status @twitter #tag"
+        assert replaceInStatus(status) == "my&status @twitter #tag"
 
+    @patch("twitter.cmdline.get_time_string", return_value="18:33:30 ")
+    def test_StatusFormatter(self, mock_get_time_string):
+        test_status = StatusFormatter()
+        options = {"timestamp": True, "datestamp": False}
+        assert test_status(self.status, options) == "18:33:30 @myusername test & test"
 
-def test_StatusFormatter():
-    test_status = StatusFormatter()
-    options = {"timestamp": True, "datestamp": False}
-    assert test_status(status, options) == "20:33:30 @myusername test & test"
+    def test_VerboseStatusFormatter(self):
+        test_status = VerboseStatusFormatter()
+        options = {"timestamp": True, "datestamp": False}
+        assert (
+            test_status(self.status, options)
+            == "-- myusername (Paris, France) on sun dec 20 18:33:30 +0000 2020\ntest & test\n"
+        )
 
-
-def test_VerboseStatusFormatter():
-    test_status = VerboseStatusFormatter()
-    options = {"timestamp": True, "datestamp": False}
-    assert (
-        test_status(status, options)
-        == "-- myusername (Paris) on sun dec 20 18:33:30 +0000 2020\ntest & test\n"
-    )
-
-
-def test_JSONStatusFormatter():
-    test_status = JSONStatusFormatter()
-    options = {"timestamp": True, "datestamp": False}
-    assert test_status(status, options) == json.dumps(
-        {
-            "created_at": "sun dec 20 18:33:30 +0000 2020",
-            "user": {"screen_name": "myusername", "location": "Paris"},
-            "text": "test & test",
-        }
-    )
+    def test_JSONStatusFormatter(self):
+        test_status = JSONStatusFormatter()
+        options = {"timestamp": True, "datestamp": False}
+        assert test_status(self.status, options) == json.dumps(
+            {
+                "created_at": "sun dec 20 18:33:30 +0000 2020",
+                "user": {"screen_name": "myusername", "location": "Paris, France"},
+                "text": "test & test",
+            }
+        )
